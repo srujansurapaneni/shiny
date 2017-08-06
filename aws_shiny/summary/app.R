4fea770dbd97836ee0ab5c67926478a01eea8ad4
srujansurapaneni <srujansurapaneni@hotmail.com>
2017-08-06 19:00:15 -0400

    added new

--- aws_shiny/summary/app.R
+++ aws_shiny/summary/app.R
@@ -1,12 +1,23 @@
 ## Only run examples in interactive R sessions
-if (interactive()) {
-  
-  ui <- fluidPage(
-    textInput("caption", "Caption", "Data Summary"),
-    verbatimTextOutput("value")
+ui <- fluidPage(
+  textAreaInput("caption", "Paste the text that needs to be summarized below", "input text here", width = "1000px"),
+  verbatimTextOutput("value")
   )
-  server <- function(input, output) {
-    output$value <- renderText({ input$caption })
+
+server <- function(input, output) {
+  output$value <- renderText({ 
+  
+                             #Start to summarize the input text here  
+                             library(algorithmia)
+                             input <- input$caption
+                             client <- getAlgorithmiaClient("simITYeMiL/Q0xcxqNBy8oM5ma71")
+                             algo <- client$algo("nlp/Summarizer/0.1.6")
+                             result <- algo$pipe(input)$result
+                             #inputvar <- input$caption 
+                             #gsub("e", "", inputvar)
+                             #genericSummary(inputvar,k=1)
+    
+                             })
   }
-  shinyApp(ui, server)
-}
+  
+shinyApp(ui, server)
