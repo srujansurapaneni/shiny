21d742050fe2aaa00f13556d976781bc5b4ea059
srujansurapaneni <srujansurapaneni@hotmail.com>
2017-08-08 08:04:30 -0400

    added new

--- aws_shiny/summary/app.R
+++ aws_shiny/summary/app.R
@@ -1,5 +1,6 @@
 ## Only run examples in interactive R sessions
 ui <- fluidPage(
+  tags$head(tags$link(rel="stylesheet",type="text/css",href="style1.css")),
   textAreaInput("caption", "Paste the text that needs to be summarized below", "input text here", width = "1000px"),
   verbatimTextOutput("value")
   )
