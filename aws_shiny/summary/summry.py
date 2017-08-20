84f46ef492500ab63cea30879c935f325509317c
srujansurapaneni <srujansurapaneni@hotmail.com>
2017-08-20 17:51:40 -0400

    added data massaging steps

--- aws_shiny/summary/summry.py
+++ aws_shiny/summary/summry.py
@@ -10,7 +10,7 @@ class SummaryTool(object):
     # Naive method for splitting a text into sentences
     def split_content_to_sentences(self, content):
         content = content.replace("\n", ". ")
-        sentences = content.split(". ;,!? ")
+        sentences = [j.strip() for j in (content.split(". "))]
         return sentences
 
     # Naive method for splitting stopwords into array
@@ -22,113 +22,21 @@ class SummaryTool(object):
         content_array = clean_content.split()
         word_count = Counter(content_array)
         most_common = word_count.most_common(5)
-        return most_common
+        imp_words = [num[0] for num in most_common]
+        return imp_words
 
-    def get_correct_summary(self,title,most_common,sentences):
-
-        # Add the title
-        summary = []
-        summary.append(title.strip())
-        summary.append("")
-
-        for s in sentences:
-            sentence_words = re.split('. ;,!?',s)
-            if (set(most_common) & set(sentence_words)):
-                summary.append(sentences)
-        return ("\n").join(summary)
-
-    #-------------------------------------------------------#
-    # Naive method for splitting a text into paragraphs
-    def split_content_to_paragraphs(self, content):
-        return content.split("\n\n")
-
-    # Caculate the intersection between 2 sentences
-    def sentences_intersection(self, sent1, sent2):
-
-        # split the sentence into words/tokens
-        s1 = set(sent1.split(" "))
-        s2 = set(sent2.split(" "))
-
-        # If there is not intersection, just return 0
-        if len(s1.intersection(s2)) == 0:
-            return 0
-
-        # We normalize the result by the average number of words
-        return len(s1.intersection(s2)) / ((len(s1) + len(s2)) / 2)
-
-    # Format a sentence - remove all non-alphbetic chars from the sentence
-    # We'll use the formatted sentence as a key in our sentences dictionary
-    def format_sentence(self, sentence):
-        sentence = re.sub(r'\W+', '', sentence)
-        return sentence
-
-    # Convert the content into a dictionary <K, V>
-    # k = The formatted sentence
-    # V = The rank of the sentence
-    def get_sentences_ranks(self, content):
-
-        # Split the content into sentences
+    def get_sentence_with_matching_words (self,content):
         sentences = self.split_content_to_sentences(content)
 
-        # Calculate the intersection of every two sentences
-        n = len(sentences)
-        values = [[0 for x in xrange(n)] for x in xrange(n)]
-        for i in range(0, n):
-            for j in range(0, n):
-                values[i][j] = self.sentences_intersection(sentences[i], sentences[j])
-
-        # Build the sentences dictionary
-        # The score of a sentences is the sum of all its intersection
-        sentences_dic = {}
-        for i in range(0, n):
-            score = 0
-            for j in range(0, n):
-                if i == j:
-                    continue
-                score += values[i][j]
-            sentences_dic[self.format_sentence(sentences[i])] = score
-        return sentences_dic
-
-    # Return the best sentence in a paragraph
-    def get_best_sentence(self, paragraph, sentences_dic):
-
-        # Split the paragraph into sentences
-        sentences = self.split_content_to_sentences(paragraph)
-
-        # Ignore short paragraphs
-        if len(sentences) < 2:
-            return ""
-
-        # Get the best sentence according to the sentences dictionary
-        best_sentence = ""
-        max_value = 0
-        for s in sentences:
-            strip_s = self.format_sentence(s)
-            if strip_s:
-                if sentences_dic[strip_s] > max_value:
-                    max_value = sentences_dic[strip_s]
-                    best_sentence = s
-
-        return best_sentence
-
-    # Build the summary
-    def get_summary(self, title, content, sentences_dic):
-
-        # Split the content into paragraphs
-        paragraphs = self.split_content_to_paragraphs(content)
+    def get_correct_summary(self,title,stopwords,content):
 
         # Add the title
         summary = []
         summary.append(title.strip())
         summary.append("")
-
-        # Add the best sentence from each paragraph
-        for p in paragraphs:
-            sentence = self.get_best_sentence(p, sentences_dic).strip()
-            if sentence:
-                summary.append(sentence)
-
-        return ("\n").join(summary)
+        sentences = self.split_content_to_sentences(content)
+        most_common = self.content_clean(stopwords,content)
+        return ' '.join(summary)
 
 # Main method, just run "python summary_tool.py"
 def main():
@@ -208,10 +116,18 @@ def main():
     # Build the sentences dictionary
     sentences = st.split_content_to_sentences(content)
 
-    # Build the summary with the sentences dictionary
-    summary = st.get_correct_summary(title, content, sentences)
+    # most commmon words
+    imp_words = st.content_clean(stopwords,content)
 
-    # Print the summary
+    # Build the summary with the sentences dictionary
+    summary = st.get_correct_summary(title, stopwords, content)
+
+    # Print the most important keywords and their frequency, title and  summary
+    print "important keywords:"
+    print imp_words
+
+    print "List of all sentences in the input:"
+    print sentences
     print summary
 
 if __name__ == '__main__':
