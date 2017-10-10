efe747d8c3dbc30e757bb0d70322a8ae1e4d7a7f
srujansurapaneni <srujansurapaneni@hotmail.com>
2017-10-10 10:14:12 -0400

    chnages

--- aws_shiny/keen_sight/test_run.py
+++ aws_shiny/keen_sight/test_run.py
@@ -1,49 +1,16 @@
 
 
-# USAGE
-# python image_diff.py --first images/original_01.png --second images/modified_01.png
+def hasAllUniqueCharsNoDS(inputstring):
+    #no data structures
+    #O(n^2)
+    for char in inputstring:
+        foundcount=0
+        for char2 in inputstring:
+            if char==char2:
+                foundcount=foundcount+1
+            if foundcount>1:
+                print("False")
+    print("True")
 
-# import the necessary packages
-from skimage.measure import compare_ssim
-import imutils
-import cv2
-
-
-
-# load the two input images
-imageA = cv2.imread('C:/Users/Root/Documents/GIT/shiny/aws_shiny/keen_sight/a.jpg')
-imageB = cv2.imread('C:/Users/Root/Documents/GIT/shiny/aws_shiny/keen_sight/b.jpg')
-
-# convert the images to grayscale
-grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
-
-# compute the Structural Similarity Index (SSIM) between the two
-# images, ensuring that the difference image is returned
-(score, diff) = compare_ssim(grayA, grayB, full=True)
-diff = (diff * 255).astype("uint8")
-print("SSIM: {}".format(score))
-
-# threshold the difference image, followed by finding contours to
-# obtain the regions of the two input images that differ
-thresh = cv2.threshold(diff, 0, 255,
-                       cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                        cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-
-# loop over the contours
-for c in cnts:
-    # compute the bounding box of the contour and then draw the
-    # bounding box on both input images to represent where the two
-    # images differ
-    (x, y, w, h) = cv2.boundingRect(c)
-    cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-# show the output images
-cv2.imshow("Original", imageA)
-cv2.imshow("Modified", imageB)
-cv2.imshow("Diff", diff)
-cv2.imshow("Thresh", thresh)
-cv2.waitKey(0)
+inputstring = "12341"
+new = hasAllUniqueCharsNoDS(inputstring)
